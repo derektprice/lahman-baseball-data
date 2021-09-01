6eb5bd810fb7f2895dc9af92687f8caf6faef120
derektprice <derektprice@gmail.com>
2021-08-31 19:28:29 -0500

    mad, but pushing to github and walking away for 5 minutes

--- scripts/derek_baseball_scripts.sql
+++ scripts/derek_baseball_scripts.sql
@@ -30,18 +30,68 @@ WHERE height IN
 	);
 
 -- Q3: Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
+-- A:
+
+--colleges code - 24 players
+-- SELECT 
+-- 	DISTINCT playerid,
+-- 	schoolid
+-- FROM collegeplaying as c
+-- WHERE schoolid = 'vandy'
+-- ORDER BY playerid;
+
+--salaries code - alvarpe01 =  20,681,704.00 
+
+-- SELECT
+-- 	s.playerid,
+-- 	s.salary AS total_salary
+-- FROM salaries as s
+-- ORDER BY s.playerid
 
 
--- A3: 
+SELECT
+	p.playerid,
+	p.namefirst,
+	p.namelast,
+	SUM(s.salary)
+FROM people as p
+LEFT JOIN salaries as s
+ON p.playerid = s.playerid
+LEFT JOIN collegeplaying as c
+ON p.playerid = c.playerid
+WHERE c.playerid IN
+	(SELECT DISTINCT playerid
+	 FROM collegeplaying
+	 WHERE schoolid = 'vandy')
+GROUP BY p.playerid
+ORDER BY playerid;
 
--- Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
--- Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
 
--- Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
 
--- From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
--- Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+-- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+-- A:
 
--- Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+-- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
+-- A:
+
+-- 6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
+-- A:
+
+
+
+-- 7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+-- A:
+
+
+
+-- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+-- A:
+
+
+
+-- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+-- A:
+
+
